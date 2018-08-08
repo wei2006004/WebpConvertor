c9e0d2909a50a2c59f0210fa04aaa05f6d024104
lizhiwei <wei2006004@foxmail.com>
2018-08-08 12:52:42 +0800

    * ui dialog

---
+++ src/main/kotlin/com/vinson/webp/MainForm.java
@@ -0,0 +1,18 @@
+package com.vinson.webp;
+
+import javax.swing.*;
+
+/**
+ * Created by Vinson on 2018/8/8.
+ * e-mail: wei2006004@foxmail.com
+ */
+public class MainForm {
+    public JPanel panel;
+    public JButton webpButton;
+    public JButton saveButton;
+    public JTextField webpText;
+    public JTextField saveText;
+    public JButton startButton;
+    public JTextPane msgText;
+    public JTextField alphaText;
+}
--- src/main/kotlin/com/vinson/webp/main.kt
+++ src/main/kotlin/com/vinson/webp/main.kt
@@ -1,20 +1,49 @@
 package com.vinson.webp
 
+import javax.swing.JFileChooser
 import javax.swing.JFrame
-import javax.swing.JPanel
 
 /**
  * Created by Vinson on 2018/8/7.
  * e-mail: wei2006004@foxmail.com
  */
+val fileChooser = JFileChooser()
+
 
 fun main(args: Array<String>) {
     val frame = JFrame("WordFrom")
-    val from = JPanel()
+    val form = MainForm()
 
-    frame.contentPane = from
+    fileChooser.fileSelectionMode = 1
+    form.webpButton.addActionListener {
+        chooseDir { success, file ->
+            if (success) {
+                form.webpText.text = file
+            }
+        }
+    }
+    form.saveButton.addActionListener {
+        chooseDir { success, file ->
+            if (success) {
+                form.saveText.text = file
+            }
+        }
+    }
+
+    frame.contentPane = form.panel
     frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
 
-    frame.setSize(700, 600)
+    frame.setSize(500, 400)
     frame.isVisible = true
+}
+
+fun chooseDir(done: (Boolean, String) -> Unit) {
+    fileChooser.showOpenDialog(null).let {
+        if (it != 1) {
+            val file = fileChooser.selectedFile
+            done(true, file.absolutePath)
+        } else {
+            done(false, "")
+        }
+    }
 }
